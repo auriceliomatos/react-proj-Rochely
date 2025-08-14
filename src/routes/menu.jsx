e2118ade5ea042c489c18056e3541e786251af7f
auriceliomatos <auriceliomatos@gmail.com>
2025-08-14 20:20:41 -0300

    corrigir erro

--- src/routes/menu.jsx
+++ src/routes/menu.jsx
@@ -4,18 +4,18 @@ import logo from "../assets/react.svg";
 export const Menu = () => {
   return (
     <>
-    <div className=" w-full bg-cyan-950  h-18 flex flex-col md:flex-row items-center justify-between rounded-xl border-1 border-green-300">
+    <div className="w-full bg-cyan-950 h-18 flex flex-col md:flex-row items-center justify-between rounded-xl border border-green-300">
 
-      <div className=" w-200 flex items-center gap-5">
+      <div className="w-auto flex items-center gap-5">
 
-        <img src={logo} alt="logo1" className="w-10 h-10 mr-2 " />
-        <h3 className="text-xl font-serif font-bold text-red-50 "> Papelaria gotinhas de amor </h3>
+        <img src={logo} alt="logo1" className="w-10 h-10 mr-2" />
+        <h3 className="text-xl font-serif font-bold text-red-50"> Papelaria gotinhas de amor </h3>
 
         
 
       </div>
 
-      <nav className=" w-40 flex gap-4">
+      <nav className="w-auto flex gap-4">
         <a href="/"         className="text-red-50 hover:text-green-300 font-medium transition-colors">Home</a>
         <a href="/contatos" className="text-red-50 hover:text-green-300 font-medium transition-colors">Contatos</a>
         
