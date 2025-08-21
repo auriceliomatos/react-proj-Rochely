feccd4bb363a352d9dab0419e130ea73f53a84d0
auriceliomatos <auriceliomatos@gmail.com>
2025-08-21 19:28:07 -0300

    update total do projeto

--- src/home/borda.jsx
+++
@@ -1,9 +0,0 @@
-//import fundo from '../../assets/f14.png'
-
-export const Borda = () => {   
-    return (
-        <div className="w-full  lg:h-3 bg-gradient-to-r from-green-200 to-green-600 " >
-
-        </div>
-    )
-}
